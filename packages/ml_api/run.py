1d5a346a4bf82addb25249a3c452106b2ff98a51
mohser sh <sh1361m@gmail.com>
2020-11-05 12:09:27 +0800

    seventh

--- packages/ml_api/run.py
+++ packages/ml_api/run.py
@@ -2,11 +2,11 @@ from api.app import create_app
 from api.config import DevelopmentConfig, ProductionConfig
 
 
-# application = create_app(
-#     config_object=ProductionConfig)
-
 application = create_app(
-    config_object=DevelopmentConfig)
+    config_object=ProductionConfig)
+
+# application = create_app(
+#     config_object=DevelopmentConfig)
 
 
 if __name__ == '__main__':
