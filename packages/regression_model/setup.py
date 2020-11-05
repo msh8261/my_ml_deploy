cd5a66e9d96e5394908170db6f03ed423c8cc9b4
mohser sh <sh1361m@gmail.com>
2020-11-05 10:58:10 +0800

    fifth

--- packages/regression_model/setup.py
+++ packages/regression_model/setup.py
@@ -11,9 +11,9 @@ from setuptools import find_packages, setup
 
 NAME = 'regression_model'
 DESCRIPTION = 'Regression model for using in the Train In Data online course "Deployment of Machine Learning Models".'
-URL = 'https://github.com/msh8261/my_ml_deploy/tree/master'
-EMAIL = 'msh@gmail.com'
-AUTHOR = 'msh'
+URL = 'https://github.com/trainindata/deploying-machine-learning-models'
+EMAIL = 'christopher.samiullah@protonmail.com'
+AUTHOR = 'ChristopherGS'
 REQUIRES_PYTHON = '>=3.6.0'
 
 
